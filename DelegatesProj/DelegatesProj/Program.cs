3cf8841b53be803c709d6a393a1a45012b4117df
vmukhametova <vmukhametova@fil-it.ru>
2024-08-11 17:39:09 +0500

    Домашнее задание по делегатам (номер 5)

--- DelegatesProj/DelegatesProj/Program.cs
+++ DelegatesProj/DelegatesProj/Program.cs
@@ -12,20 +12,19 @@ namespace DelegatesProj
             {
                 Console.WriteLine("Введите полный путь до директории. Например C:\\, либо нажмите Enter, чтобы оставить текущую директорию.");
                 var dirPath = Console.ReadLine();
-                Console.WriteLine("Введите шаблон поиска файлов. Например *.jpg|*.png|*.gif, либо нажмите Enter.");
+                Console.WriteLine("Введите шаблон поиска файлов. Например *.jpg или Picture.*, либо нажмите Enter.");
                 var pattern = Console.ReadLine();
+                Console.WriteLine("Расширение, на котором завершить обработку. Например .txt, либо нажмите Enter, если таковые не нужны.");
+                var ext = Console.ReadLine();
 
                 if (String.IsNullOrEmpty(dirPath))
                     dirPath = Directory.GetCurrentDirectory();
 
-                var ext = "";
                 if (String.IsNullOrEmpty(pattern))
-                {
                     pattern = "*";
-                    Console.WriteLine("Расширение, на котором завершить обработку. Например .txt, либо нажмите Enter, если таковые не нужны.");
-                    ext = Console.ReadLine();
-                }
+                
                 Console.WriteLine();
+
                 test.RunProcess(dirPath, pattern, ext);
 
                 Console.WriteLine($"\n--------------------------------------------------------\n");
